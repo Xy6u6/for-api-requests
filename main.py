953cc3f4d8dc17d7176c8ce5d5d49bab81dd678a
xy6u6 <Anton.voroniuk@gmail.com>
2021-11-19 16:53:45 +0200

    add function (get_info) getting info about FC in different seasons

--- main.py
+++ main.py
@@ -1,15 +1,41 @@
 import requests
 import json
 import pandas as pd
-url = "https://v3.football.api-sports.io/teams/statistics?season=2021&team=550&league=333"
 
-payload={}
+url = "https://v3.football.api-sports.io/teams/statistics?season=2020&team=550&league=333"
+
+payload = {}
 headers = {
-  'x-rapidapi-key': '254205446bea705afd5d227e7dd696c5',
-  'x-rapidapi-host': 'v3.football.api-sports.io'
+    'x-rapidapi-key': '254205446bea705afd5d227e7dd696c5',
+    'x-rapidapi-host': 'v3.football.api-sports.io'
 }
+seasons = ['2018', '2019', '2020', '2021']
 
-response = requests.request("GET", url, headers=headers, data=payload)
-data = response.json()
-with open('tmp/res.json', 'w') as f:
-     json.dump(data, f, indent=4)
+
+def get_info():
+    for i in seasons:
+        url = f'https://v3.football.api-sports.io/teams/statistics?season={i}&team=550&league=333'
+        response = requests.request("GET", url, headers=headers, data=payload)
+        data = response.json()
+        with open(f'tmp/res{i}.json', 'w') as f:
+            json.dump(data, f, indent=4)
+
+
+def get_win(year):
+    with open(f'tmp/res{str(year)}.json', 'r') as f:
+        data = json.load(f)
+        fc_name = data['response']['team']['name']
+        fc_wins = data['response']['fixtures']['wins']['total']
+        fc_loses = data['response']['fixtures']['loses']['total']
+        fc_draws = data['response']['fixtures']['draws']['total']
+        print(fc_name)
+    pass
+
+
+def get_loses():
+    pass
+
+
+if __name__ == '__main__':
+    # get_info()
+    get_win(2020)
