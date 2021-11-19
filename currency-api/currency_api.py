b2e221e7e33baef5e006d14c117143bff21c6ef5
xy6u6 <Anton.voroniuk@gmail.com>
2021-11-19 23:15:04 +0200

    added func get_max_rate

--- currency-api/currency_api.py
+++ currency-api/currency_api.py
@@ -1,6 +1,7 @@
 import datetime
 import json
 import requests
+import os
 
 
 def get_currency(years):
@@ -12,9 +13,25 @@ def get_currency(years):
         headers = {}
         response = requests.request("GET", url, headers=headers, data=payload)
         data = response.json()
-        today = today.replace(year=today.year - 1)
         with open(f'json_data/resp_cur{today.year}.json', 'w') as f:
             json.dump(data, f, indent=4)
+        today = today.replace(year=today.year - 1)
+
+
+def get_max_rate():
+    # TODO: FIX 10
+    list_of_dates = []
+    list_of_rates = []
+    listoffiles = list((os.listdir(f'{os.getcwd()}/json_data')))
+    for i in listoffiles:
+        with open(f'json_data/{i}') as f:
+            data = json.load(f)
+            list_of_rates.append(data['rates']['UAH'])
+            list_of_dates.append(data['date'])
+            print(data['date'], data['rates']['UAH'])
+    maxrate =list_of_rates.index(max(list_of_rates))
+    print(f'the max rate was in {list_of_dates[maxrate]} year, is {max(list_of_rates)}')
 
 if __name__ == '__main__':
-    get_currency(5)
+    # get_currency(15)
+    get_max_rate()
