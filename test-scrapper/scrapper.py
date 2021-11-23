a0c9ab51eb2b759d5ec73881fe1cd491bfa801c2
xy6u6 <Anton.voroniuk@gmail.com>
2021-11-23 16:39:10 +0200

    get_res

--- test-scrapper/scrapper.py
+++ test-scrapper/scrapper.py
@@ -2,14 +2,17 @@ import requests
 from bs4 import BeautifulSoup
 
 def get_res():
-    url = 'https://quotes.toscrape.com/'
+    url = 'https://quotes.toscrape.com'
     res = requests.get(url)
     sp = BeautifulSoup(res.text, 'lxml')
-    tags = sp.findAll('span', class_='tag-item')
-    quotes = sp.findAll('span', class_='text')
-    authors = sp.findAll('small', class_='author')
-    print(tags[0])
-    # for i, k in enumerate(quotes):
-    #     print(f'{i+1}. {k.text} by: {authors[i].text}')
+    tags = sp.find('div', {"class": 'col-md-4 tags-box'}).find_all('a', class_='tag')
+    for z, i in enumerate(tags):
+        res = requests.get(f'{url}{i.get("href")}')
+        sp = BeautifulSoup(res.text, 'lxml')
+        quotes = sp.findAll('span', class_='text')
+        authors = sp.findAll('small', class_='author')
+        for l, k in enumerate(quotes):
+            with open(f'{z+1} list of {tags[z].text} sentences.txt', 'a') as f:
+             f.write(f'{l+1}. {k.text} by: {authors[l].text} \n')
 if __name__ == '__main__':
     get_res()
